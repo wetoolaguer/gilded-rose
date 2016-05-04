863c7d0f0f4be6dc679a250a2b774e494c385c36
wetoolaguer <wenceslaoolaguer@gmail.com>
2016-05-04 18:10:33 +0800

    Finishes test

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -3,11 +3,113 @@ defmodule GildedRose do
   # update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 1}])
   # => [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 3}]
 
+  @backstage_pass "Backstage passes to a TAFKAL80ETC concert"
+  @upgrades_by_age [@backstage_pass, "Aged Brie"]
+  @legendaries ["Sulfuras, Hand of Ragnaros"]
+  @conjured ["+5 Dexterity Vest", "Elixir of the Mongoose"]
+  @min_quality 0
+  @max_quality 50
+
   def update_quality(items) do
     Enum.map(items, &update_item/1)
   end
 
   def update_item(item) do
+    cond do
+      unsellable?(item) -> item
+      unmodifiable?(item) -> item
+      conjured?(item) -> downgrade_conjured(item)
+      upgrades_by_age?(item) -> upgrade_item(item)
+      true -> downgrade_item(item)
+    end
+  end
+
+  defp upgrade_item(item = %{name: name}) when name == @backstage_pass do
+    cond do
+      new?(item) -> upgrade_by(item, 2)
+      old?(item) -> upgrade_by(item, 3)
+      expired?(item) -> downgrade_by(item, item.quality)
+      true -> item
+    end
+  end
+
+  defp upgrade_item(item) do
+    cond do
+      new?(item) -> upgrade_by(item, 1)
+      old?(item) -> upgrade_by(item, 1)
+      expired?(item) -> item
+      true -> item
+    end
+  end
+
+  defp downgrade_item(item)  do
+    cond do
+      new?(item) -> downgrade_by(item, 1)
+      old?(item) -> downgrade_by(item, 1)
+      expired?(item) -> downgrade_by(item, 2)
+      true -> item
+    end
+  end
+
+  defp downgrade_conjured(item) do
+    cond do
+      new?(item) -> downgrade_by(item, 2)
+      old?(item) -> downgrade_by(item, 2)
+      expired?(item) -> downgrade_by(item, 4)
+      true -> item
+    end
+  end
+
+  defp upgrade_by(item, count) do
+    new_val = item.quality + count
+    new_val = if new_val > 50,  do: 50, else: new_val
+    %{item | quality: new_val }
+  end
+
+  defp downgrade_by(item, count) do
+    new_val = item.quality - count
+    new_val = if new_val < 0, do: 0, else: new_val
+    %{item | quality: new_val }
+  end
+
+  defp new?(item) do
+    item.sell_in > 5 && item.sell_in <= 10
+  end
+
+  defp old?(item) do
+    item.sell_in >= 0 && item.sell_in <= 5
+  end
+
+  defp expired?(item) do
+    item.sell_in < 0
+  end
+
+  defp upgrades_by_age?(item) do
+    Enum.member?(@upgrades_by_age, item.name)
+  end
+
+  defp unsellable?(item) do
+    Enum.member?(@legendaries, item.name)
+  end
+
+  defp unmodifiable?(item) do
+    max?(item) || broken?(item)
+  end
+
+  defp max?(item) do
+    item.quality >= 50
+  end
+
+  defp broken?(item) do
+    item.quality <= 0
+  end
+
+  defp conjured?(item) do
+    Enum.member?(@conjured, item.name)
+  end
+
+  #old implementation
+  def update_item_2(item) do
     cond do
       #done
       item.quality == 0 ->
@@ -50,6 +152,7 @@ defmodule GildedRose do
 
       item.name == "+5 Dexterity Vest" || item.name == "Elixir of the Mongoose" ->
         %{item | quality: item.quality - 2}
+
       item.name != "Sulfuras, Hand of Ragnaros" ->
         %{item | quality: item.quality - 1}
       true ->
--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -24,9 +24,57 @@ defmodule GildedRoseTest do
   @conjured %Item{
     name: "+5 Dexterity Vest",
     sell_in: 9,
-    quality: 1
+    quality: 10
   }
 
+  @normal %Item{
+    name: "Normal Item",
+    sell_in: 10,
+    quality: 10
+  }
+
+  ### True tests
+  test "update_item returns unsellable items" do
+    assert GildedRose.update_item(@sulfuras) == @sulfuras
+  end
+
+  test "update_item downgrades item if downgradable" do
+    broken = %{@normal | quality: 0}
+    new = %{@normal | sell_in: 10}
+    old = %{@normal | sell_in: 0}
+    expired = %{@normal | sell_in: -1}
+
+    assert GildedRose.update_item(broken) == broken
+    assert GildedRose.update_item(new).quality == old.quality - 1
+    assert GildedRose.update_item(old).quality == old.quality - 1
+    assert GildedRose.update_item(expired).quality == expired.quality - 2
+  end
+
+  test "update_item upgrades backstage_pass if upgradable" do
+    max = %{@backstage_pass | quality: 50}
+    new = %{@backstage_pass | sell_in: 10}
+    old = %{@backstage_pass | sell_in: 0}
+    expired = %{@backstage_pass | sell_in: -1}
+
+    assert GildedRose.update_item(max) == max
+    assert GildedRose.update_item(new).quality == old.quality + 2
+    assert GildedRose.update_item(old).quality == old.quality + 3
+    assert GildedRose.update_item(expired).quality == 0
+  end
+
+  test "update_item downgrades conjured twice as fast" do
+    broken = %{@conjured | quality: 0}
+    new = %{@conjured | sell_in: 10}
+    old = %{@conjured | sell_in: 0}
+    expired = %{@conjured | sell_in: -1}
+
+    assert GildedRose.update_item(broken) == broken
+    assert GildedRose.update_item(new).quality == old.quality - 2
+    assert GildedRose.update_item(old).quality == old.quality - 2
+    assert GildedRose.update_item(expired).quality == expired.quality - 4
+  end
+
+  ### Handles
   test "returns item if quality == 0" do
     item = %{@backstage_pass | quality: 0}
     assert GildedRose.update_item(item) == item
@@ -63,8 +111,8 @@ defmodule GildedRoseTest do
   end
 
   test "Conjured: quality - 2 if sell_in < 0" do
-    item = %{@conjured | sell_in: -1}
-    assert GildedRose.update_item(item).quality == item.quality - 2
+    item = %{@conjured | sell_in: -1, quality: 10}
+    assert GildedRose.update_item(item).quality == item.quality - 4
   end
 
   test "NonConjured and NonBackstage: returns item if sell_in < 0" do
@@ -72,7 +120,7 @@ defmodule GildedRoseTest do
     assert GildedRose.update_item(item) == item
   end
   
-  test "Conjured: quality - 1 if sell_in > 0" do
+  test "Conjured: quality - 2 if sell_in > 0" do
     item = %{@conjured | sell_in: 5, quality: 5}
     assert GildedRose.update_item(item).quality == item.quality - 2
   end
